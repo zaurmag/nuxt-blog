5f54b257dbe62d6b77eccb9c6c2fe12c423ee24b
zaurmag411@gmail.com <guKGyVmC7o>
2023-06-05 21:52:20 +0300

    Fix: minor edits

--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGO_URI: 'mongodb://localhost:27017/nuxt-blog',
-  JWT: 'dev-jwt-key',
+  JWT: 'dev-jwt-key'
   // MONGO_URI: 'mongodb://zaurmag:zaur4495544@localhost:27017/nuxt_blog_db'
 }
